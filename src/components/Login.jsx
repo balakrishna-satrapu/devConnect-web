48695d5eb8b0e872ea147db2cf6c28a91dbf7647
Balakrishna <balakrishnasatrapu@gmail.com>
2026-04-26 22:35:10 +0530

    feat: signup

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -7,7 +7,10 @@ import { useNavigate } from "react-router-dom";
 const Login = () => {
   const [emailId, setEmailId] = useState("");
   const [password, setPassword] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [error, setError] = useState("");
+  const [isSignup, setIsSignup] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -25,10 +28,48 @@ const Login = () => {
     }
   };
 
+  const handleSignupClick = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:7777/signup",
+        { firstName, lastName, emailId, password },
+        { withCredentials: true },
+      );
+      dispatch(addUser(res.data));
+      return navigate("/profile/edit");
+    } catch (err) {
+      setError(err?.response?.data?.message);
+    }
+  };
+
   return (
     <div className="w-screen h-[80vh] flex items-center justify-center">
       <fieldset className="fieldset bg-base-300 border-base-300 rounded-box w-xs border p-6">
-        <p className="text-lg flex justify-center">Login</p>
+        <p className="text-lg flex justify-center">{isSignup ? "Signup" : "Login"}</p>
+        
+        {isSignup && <div>
+          <label className="mt-2 label">FirstName</label>
+          <input
+            type="text"
+            className="input"
+            placeholder="FirstName"
+            value={firstName}
+            onChange={(event) => {
+              setFirstName(event.target.value);
+            }}
+          />
+          
+          <label className="mt-2 label">LastName</label>
+          <input
+            type="text"
+            className="mb-2 input"
+            placeholder="LastName"
+            value={lastName}
+            onChange={(event) => {
+              setLastName(event.target.value);
+            }}
+          />
+        </div>}
 
         <label className="mt-2 label">Email Id</label>
         <input
@@ -52,8 +93,14 @@ const Login = () => {
           }}
         />
         <p className="text-sm text-red-400">{error}</p>
-        <button className="btn btn-primary my-2" onClick={handleLoginClick}>
+        <button className="btn btn-primary my-2" onClick={isSignup ? handleSignupClick : handleLoginClick}>
           Submit
+        </button>
+        <button 
+          className="cursor-pointer"
+          onClick={() => setIsSignup(!isSignup)}
+          >
+            {isSignup ? "Have an account? Login" : "Signup"}
         </button>
       </fieldset>
     </div>
